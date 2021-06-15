5403e9d39a9611356d6e2f2c0c715191c8d717ed
Claire <a0505kimo@hotmail>
2021-06-15 15:55:49 +0800

    將DetailFragment 顯示是否為出租件或是賣件的種類和價格 1.打開detail/DetailViewModel.kt 導入androidx.lifecycle.Transformations。
    // The displayPropertyPrice formatted Transformation Map LiveData, which displays the sale
        // or rental price.
        val displayPropertyPrice = Transformations.map(selectedProperty){
            app.applicationContext.getString(
                when (it.isRental){
                    true -> R.string.display_price_monthly_rental
                    false -> R.string.display_price
                }, it.price)
        }
    
        // The displayPropertyType formatted Transformation Map LiveData, which displays the
        // "For Rent/Sale" String
        val displayPropertyType = Transformations.map(selectedProperty) {
            app.applicationContext.getString(
                when(it.isRental){
                    true -> R.string.type_rent
                    false -> R.string.type_sale
                })
        }
    }
    
    打開res/layout/fragment_detail.xml
    <TextView
       android:id="@+id/property_type_text"
    ...
    android:text="@{viewModel.displayPropertyType}"
    ...
       tools:text="To Rent" />
    
    <TextView
       android:id="@+id/price_value_text"
    ...
    android:text="@{viewModel.displayPropertyPrice}"
    ...
       tools:text="$100,000" />

--- app/src/main/java/com/example/marsrealestate/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/marsrealestate/detail/DetailViewModel.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import com.example.marsrealestate.R
 import com.example.marsrealestate.network.MarsProperty
 
 /**
@@ -23,4 +25,23 @@ class DetailViewModel(marsProperty: MarsProperty, app: Application) : AndroidVie
         _selectedProperty.value = marsProperty
     }
 
+    // The displayPropertyPrice formatted Transformation Map LiveData, which displays the sale
+    // or rental price.
+    val displayPropertyPrice = Transformations.map(selectedProperty){
+        app.applicationContext.getString(
+            when (it.isRental){
+                true -> R.string.display_price_monthly_rental
+                false -> R.string.display_price
+            }, it.price)
+    }
+
+    // The displayPropertyType formatted Transformation Map LiveData, which displays the
+    // "For Rent/Sale" String
+    val displayPropertyType = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(
+            when(it.isRental){
+                true -> R.string.type_rent
+                false -> R.string.type_sale
+            })
+    }
 }
